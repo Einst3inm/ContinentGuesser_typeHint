cc82e0dba6b512adc0392de612cc9f14be7449ef
Einst3inm <einsteinmwase@gmail.com>
2026-02-12 23:46:22 -0800

    Updated Country and Continent lists

--- Etc III (Type Hints).py
+++ Etc III (Type Hints).py
@@ -16,11 +16,32 @@ Thursday, 12th February, 2026
 #So "North America" or "north america" or "Northamerica" or "northamerica", are all be correct for the country "United States of America"
 #As an execution of "type hints" from John Mallan
 
-America = ["USA", "Canada", "Mexico", "Brazil", "Argentina"]
-Africa = ["Nigeria", "Egypt", "South Africa", "Kenya", "Ethiopia"]
-Asia = ["China", "India", "Japan", "South Korea", "Indonesia"]  
-Europe = ["United Kingdom", "Germany", "France", "Italy", "Spain"]
-Australia = ["Australia", "New Zealand", "Fiji", "Papua New Guinea"]
+North_America = ["American Samoa", "Northern Mariana Islands", "Wake Island", "Guam", "Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada", "Costa Rica", 
+                "Cuba", "Dominica", "Dominican Republic", "El Salvador", "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Panama",
+                "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Trinidad and Tobago", "United States of America", "US Virgin Islands",
+                "Puerto Rico", "Navassa Island"]
+South_America = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"]
+Africa = ["Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros", 
+          "Democratic Republic of Congo", "Republic of Congo", "Cote d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Eswatini", "Ethiopia", "Gabon", 
+          "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius", "Morocco", 
+          "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda", "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", 
+          "South Sudan", "Sudan", "Tanzania", "Togo", "Tunisia", "Uganda", "Zambia", "Zimbabwe"]
+Asia = ["Afghanistan", "Armenia", "Azerbaijan", "Bahrain", "Bangladesh", "Bhutan", "Brunei", "Cambodia", "China", "Cyprus", "Georgia", "India", "Indonesia", "Iran", 
+        "Iraq", "Israel", "Japan", "Jordan", "Kazakhstan", "Kuwait", "Kyrgyzstan", "Laos", "Lebanon", "Malaysia", "Maldives", "Mongolia", "Myanmar", "Nepal",
+        "North Korea", "Oman", "Pakistan", "Palestine", "Philippines", "Qatar", "Russia", "Saudi Arabia", "Singapore", "South Korea","Sri Lanka", "Syria", "Taiwan", 
+        "Tajikistan", "Thailand", "Timor-Leste", "Turkey", "Turkmenistan", "United Arab Emirates", "Uzbekistan", "Vietnam", "Yemen"]
+Europe = ["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czechia", 
+          "Denmark", "Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kazakhstan", "Kosovo", "Latvia", 
+          "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Monaco", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", 
+          "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "United Kingdom", "Vatican City",
+          "French Polynesia", "Wallis and Futuna", "New Caledonia", "Pitcairn Islands", "Turks and Caicos Islands", "Sint Maarten", "Saba", "Saint Barthelemy", 
+          "Saint Martin", "Saint Pierre and Miquelon", "Sint Eustatius", "Montserrat", "Martinique", "Guadeloupe ", "Greenland", "Curacao", "Cayman Islands", 
+          "Clipperton Island", "Anguilla", "Aruba", "Bermuda", "Bonaire", "British Virgin Islands", "Falkland Islands", "French Guiana", 
+          "South Georgia and the South Sandwich Islands"]
+Australia = ["Australia", "Fiji", "Kiribati", "Marshall Islands", "Micronesia", "Nauru", "New Zealand", "Palau", "Papua New Guinea", "Samoa", "Solomon Islands", 
+             "Tonga", "Tuvalu", "Vanuatu", "Cook Islands", "Tokelau", "Niue", "Norfolk Island"]
+
+#The source of the list of Asian countries is : https://www.countries-ofthe-world.com/countries-of-asia.html
 
 import random
 
